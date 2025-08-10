455a4b8d4344d79bc65cf010ea2b438305efb8a4
theadotk <131266273+theadotk@users.noreply.github.com>
2025-08-10 09:04:59 +0530

    added a few remaps

---
+++ lua/config/remap.lua
@@ -0,0 +1,8 @@
+vim.keymap.set("n", "<space>ww", "<cmd>w<CR>")
+vim.keymap.set("n", "<space>qq", "<cmd>q!<CR>")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.keymap.set("n", "<space>nt", "<cmd>Ex<CR>")
